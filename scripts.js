5504ac4d8d806186ab5bbb92e5ad9a787a7faa20
vcolavin <vcolavin@gmail.com>
2019-02-17 16:41:50 -0800

    improve styling a bit more

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
 window.onload = runTheCode;
 
+const faceAnimationDuration = 0.75;
+
+document.documentElement.style.setProperty(
+	'--face-animation-duration',
+	`${faceAnimationDuration}s`
+);
+
 function runTheCode() {
 	const face = document.querySelector('#face');
 	const topHalf = document.querySelector('#top-half');
 	const bottomHalf = document.querySelector('#bottom-half');
 
-	face.onclick = () => {
-		bottomHalf.classList.toggle('open');
-		topHalf.classList.toggle('open');
-	};
+	let faceOpen = false;
+
+	face.addEventListener('click', handleFaceClick);
+
+	function handleFaceClick() {
+		if (faceOpen) {
+			return;
+		}
+
+		faceOpen = true;
+
+		face.classList.toggle('open');
+
+		// window.setTimeout(() => {
+		// 	faceTransitioning = false;
+		// }, faceAnimationDuration * 1000);
+	}
 }
